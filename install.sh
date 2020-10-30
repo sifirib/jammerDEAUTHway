f4a92919e2923b336350e340154a0a8c104b468d
hsrv <ahusrevceker@gmail.com>
2020-10-30 14:50:18 +0300

    Several package managers have been added.

--- install.sh
+++ install.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
-#login as root and run this script as root.
+#Run this script as root.
 
-apt-get update
-apt-get install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+APT_GET=$(which apt-get)
+APT=$(which apt)
+YUM=$(which yum)
+DNF=$(which dnf) 
+PKG=$(which pkg)
+MAKE=$(which make)
+
+if [[ ! -z $YUM ]]; then
+  yum check-update
+  yum install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  exit 1;
+elif [[ ! -z $APT_GET ]]; then
+  apt-get update
+  apt-get install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  exit 1;
+elif [[ ! -z $APT ]]; then
+  apt update
+  apt install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  exit 1;
+elif [[ ! -z $DNF ]]; then
+  dnf check-update
+  dnf install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  exit 1;
+elif [[ ! -z $PKG ]]; then
+  pkg update
+  pkg install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  exit 1;
+else
+  echo "Cannot install packages! Try install them manually."
+  exit 1;
+fi
+
