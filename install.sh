4bc6459e0bac5d134d79665690c8794ea8882624
hsrv <ahusrevceker@gmail.com>
2020-11-05 02:53:15 +0300

    Added several package managers...

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #Run this script as root.
 
+PACKAGES = "python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal"
+
 APT_GET=$(which apt-get)
 APT=$(which apt)
 YUM=$(which yum)
@@ -10,27 +12,26 @@ MAKE=$(which make)
 
 if [[ ! -z $YUM ]]; then
   yum check-update
-  yum install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  yum install PACKAGES
   exit 1;
 elif [[ ! -z $APT_GET ]]; then
   apt-get update
-  apt-get install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  apt-get install $PACKAGES
   exit 1;
 elif [[ ! -z $APT ]]; then
   apt update
-  apt install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
-  exit 1;
+  apt install python3 $PACKAGES
 elif [[ ! -z $DNF ]]; then
   dnf check-update
-  dnf install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  dnf install python3 $PACKAGES
   exit 1;
 elif [[ ! -z $PKG ]]; then
   pkg update
-  pkg install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  pkg install python3 $PACKAGES
   exit 1;
 elif [[ ! -z $MAKE ]]; then
   make update
-  make install python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal
+  make install python3 $PACKAGES
   exit 1;
 else
   echo "Cannot install packages! Try install them manually."
