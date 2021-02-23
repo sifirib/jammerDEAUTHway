3c0d3a43e6bceb126e37f8dfefdd98c94cce3c80
hsrv <sifirib01@gmail.com>
2021-02-23 21:41:11 +0300

    More stabilized.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Run this script as root.
-
-PACKAGES = "python3 sqlite3 net-tools aircrack-ng sqlite3 gnome-terminal"
 
 APT_GET=$(which apt-get)
 APT=$(which apt)
@@ -9,32 +7,36 @@ YUM=$(which yum)
 DNF=$(which dnf) 
 PKG=$(which pkg)
 MAKE=$(which make)
+PACMAN=$(which pacman)
 
 if [[ ! -z $YUM ]]; then
   yum check-update
-  yum install PACKAGES
+  yum install - < requirements.txt
   exit 1;
 elif [[ ! -z $APT_GET ]]; then
   apt-get update
-  apt-get install $PACKAGES
+  apt-get install - < requirements.txt
   exit 1;
 elif [[ ! -z $APT ]]; then
   apt update
-  apt install python3 $PACKAGES
+  apt install - < requirements.txt
 elif [[ ! -z $DNF ]]; then
   dnf check-update
-  dnf install python3 $PACKAGES
+  dnf install - < requirements.txt
   exit 1;
 elif [[ ! -z $PKG ]]; then
   pkg update
-  pkg install python3 $PACKAGES
+  pkg install - < requirements.txt
+  exit 1;
+elif [[ ! -z $PACMAN ]]; then
+  pacman -Syu  
+  pacman -S - < requirements.txt
   exit 1;
 elif [[ ! -z $MAKE ]]; then
   make update
-  make install python3 $PACKAGES
+  make install - < requirements.txt
   exit 1;
 else
-  echo "Cannot install packages! Try install them manually."
+  echo "Cannot install the packages! Try to install them manually."
   exit 1;
 fi
-
